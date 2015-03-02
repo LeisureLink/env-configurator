35039cb24d5efd659a29b18ef86b056f96f2ed66
Michael Hughes <mhughes@leisurelink.com>
2015-03-02 09:46:18 -0700

    correct parse errors for strict ecma script 5, start adding tests for lib module

---
+++ test/lib.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+var expect = require('expect'),
+    underTest = require('../lib/index.js'),
+    AssertionError = require('assert').AssertionError,
+    assert = require('assert');
+
+describe('env-configurator lib', function() {
+  it('should throw an error if given an undefined config object or config was not an object', function () {
+    try {
+      underTest(undefined, function () { });
+      throw new Error("Should not have passed error catch blocks");
+    } catch (err) {
+      expect(err instanceof AssertionError).toBe(true);
+    }
+    try {
+      underTest('foo', function () { });
+      throw new Error("Should not have passed error catch blocks");
+    } catch (err) {
+      expect(err instanceof AssertionError).toBe(true);
+    }
+  });
+
+  it('should throw an error if given an undefined callback function', function () {
+    try {
+      underTest({}, null);
+      throw new Error("Should not have passed error catch blocks");
+    } catch (err) {
+      expect(err instanceof AssertionError).toBe(true);
+    }
+    try {
+      underTest({}, 'foo');
+      throw new Error("Should not have passed error catch blocks");
+    } catch (err) {
+      expect(err instanceof AssertionError).toBe(true);
+    }
+  });
+})
