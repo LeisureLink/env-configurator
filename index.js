f37c7ec53b47351dfe070daaebdff05066cb69d3
Michael Hughes <mhughes@leisurelink.com>
2015-03-03 11:52:29 -0700

    comment cleanup

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-﻿/**
- * Exports the components from the env-configurator library
- * @param {object} callback - A callback function provided by the application in case it wants notification of when configuration is complete
- * and the configuration data
- * @module env-configurator
- */
-module.exports = (function () {
+﻿module.exports = (function () {
   'use strict';
   var JaySchema = require('jayschema'),
       jaySchema = new JaySchema(),
@@ -20,10 +14,16 @@ module.exports = (function () {
     jaySchema.validate(configSpec, schema, done);
   }
   
+  /**
+   * Creates a configurator object
+   * @name module:env-configurator
+   * @constructor
+   */
   function Configurator() { }
   
   /**
    * Refreshes a specific configuration by config spec name from all valid configuration sources
+   * @name module:env-configurator#renew
    * @param {string} context - The name of the config spec, specifically the 'name' property of the originally given configuration spec
    * @param {configFulFilled} [configFulfilled] - An optional callback function that notifies the client when configuration work is complete
    */
@@ -50,6 +50,7 @@ module.exports = (function () {
   
   /**
    * Refreshes the configurators existing configuration specs from all valid configuration sources
+   * @name module:env-configurator#renewAll
    * @param {configFulFilled} [configFulfilled] - An optional callback function that notifies the client when configuration work is complete
    */
   Object.defineProperty(Configurator.prototype, 'renewAll', {
@@ -89,7 +90,7 @@ module.exports = (function () {
   
   /**
    * Returns a configuration property from a particular configuration context as identified by a JSON-ptr
-   * @name module:env-configurator#configurator#get
+   * @name module:env-configurator#get
    * @param {string} context - The configurator context, this name corresponds to the configuration spec name
    * @param {string} ptrStr - A @{link http://tools.ietf.org/html/rfc6901|JSON ptr} string to the value the client needs
    * @returns The requested value or undefined
@@ -114,7 +115,7 @@ module.exports = (function () {
    * Attempts to fulfill a client's configuration specification by retrieve configuration from various sources and making it
    * available via the get method. Note that only the first of multiple calls to this method with a configuration spec will be
    * acted on, calls after the first will be ignored. To update an existing configuration call renewAll or renew('name')
-   * @name module:env-configurator#configurator#fulfill
+   * @name module:env-configurator#fulfill
    * @param {object} configSpec - A config object described by the @{link file://lib/configuration-schema.json|the configurator's JSON schema}
    * @param {configFulfilled} [configFulfilled] - An optional callback function that will be called when successful configuration is complete or when an error is encountered
    */
